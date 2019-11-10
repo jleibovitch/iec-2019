ea6b18b2d56392c6d51cd547f5cc89b9153a1aac
Jamieson Leibovitch <leibovja@mc02vt0hphtd6.prd.mpac.ca>
2019-11-10 12:41:32 -0500

    added extra apis

--- server.py
+++ server.py
@@ -5,6 +5,56 @@ import json
 app = Flask(__name__)
 connection = create_connection()
 
+
+@app.route('/api/business/signin', methods=["POST"])
+def get_business():
+
+
+    data = request.get_json()
+
+    cursor = connection.cursor()
+
+    cursor.execute('select * from business where username=%s and password=%s', (data["username"], data["password"]))
+    res = cursor.fetchone()
+
+    status = 200 if res is not None else 404
+    responseData = res if res is not None else {}
+
+    jsonResponse = {
+        "status": status,
+        "response": responseData
+    }
+
+    cursor.close()
+
+    return jsonify(jsonResponse)
+
+@app.route('/api/business/signup', methods=["POST"])
+def create_business():
+
+    try:
+
+        payload = request.get_json()
+
+        cursor = connection.cursor()
+        cursor.execute('insert into business(name, location, username, password, rating, email) values(%s, %s, %s, %s, %s, %s, %s)',
+            (payload["name"], payload["location"], payload["username"], payload["password"], payload["rating"], payload["email"]))
+
+        connection.commit()
+
+        jsonResponse = {
+            "status": "200",
+        }
+        cursor.close()
+    except Exception as e:
+        print(e)
+        jsonResponse = {
+            "status": "400",
+        }
+
+
+    return jsonify(jsonResponse)
+
 @app.route('/api/user/signin', methods=["POST"])
 def get_user():
 
@@ -24,6 +74,7 @@ def get_user():
         "response": responseData
     }
 
+    cursor.close()
     return jsonify(jsonResponse)
 
 @app.route('/api/user/signup', methods=["POST"])
@@ -42,6 +93,7 @@ def create_user():
         jsonResponse = {
             "status": "200",
         }
+        cursor.close()
     except Exception as e:
         print(e)
         jsonResponse = {
@@ -49,6 +101,74 @@ def create_user():
         }
 
     return jsonify(jsonResponse)
+
+@app.route('/api/tasks/<userid>', methods=["GET"])
+def get_user_jobs(userid):
+
+    sql = """
+    select b.name business_name, b.location, t.status task_status, j.description job_description, j.reward, j.title
+    from task t
+    join business b
+      on b.id = t.business_id
+    join job j
+      on j.id = t.job_id
+    where user_id=%s
+    """
+
+    cursor = connection.cursor()
+    cursor.execute(sql, (userid))
+
+    jsonResponse = {
+            "status": "200",
+            "response": [r[0] for r in cursor.fetchall()]
+    }
+
+    cursor.close()
+
+@app.route('/api/tasks/create', methods=["POST"])
+def add_task():
+    try:
+        payload = request.get_json()
+        cursor = connection.cursor()
+        cursor.excute("insert into task values(%s, %s, %s, %s)", payload['business_id'], payload['user_id'], '1', payload['job_id'])
+        cursor.close()
+
+        jsonResponse = {
+            "status": "200",
+        }
+
+    except Exception as e:
+        print(e)
+        jsonResponse = {
+            "status": "400",
+        }
+
+
+
+@app.route('/api/jobs/job', methods=["POST"])
+def create_job():
+
+    try:
+        payload = request.get_json()
+        cursor = connection.cursor()
+        cursor.execute("insert into job(business_id, reward, description, location) values (%s, %s, %s, %s)",
+        (payload["business_id"], payload["reward"], payload["description"], payload["location"]))
+        cursor.commit()
+        cursor.close()
+
+        jsonResponse = {
+            "status": "200",
+        }
+    except Exception as e:
+        print(e)
+        jsonResponse = {
+            "status": "400",
+        }
+
+    return jsonify(jsonResponse)
+
+
+
 
 if __name__ == '__main__':
     app.debug = True
